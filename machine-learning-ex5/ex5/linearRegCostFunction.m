5aad81426e74fe106dd911b8f26ede2739c59f25
satznova <satznova@gmail.com>
2019-06-29 19:07:29 +0530

    implemented reg lin regression function

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,14 +19,22 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% LINEAR REGRESSION: REGULARISED COST FUNCTION
+
+% lamda : Regularisation parameter helps prevent overfitting by keeping all thetas small and smoothens the curve.
+%         Else, some thetas can assume large value and some small values, and may lead to overfitting.
+%         By using Regularisation, we are making sure all thetas are small and all features given equal importance,
+%         since its hard to say which feature is importatnt.
+
+
+cost_term = sum( ((X*theta) - y).^2 )
+reg_term  = lambda * sum(theta(2:end).^2)
+
+J = (1/(2*m)) * (cost_term + reg_term)
 
 
 
-
-
-
-
-
+% LINEAR REGRESSION: REGULARISED GRADIENT FUNCTION
 
 
 
