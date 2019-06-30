7017548a5531d5dc735e744ee5e02d3d73a45837
satznova <satznova@gmail.com>
2019-06-30 07:50:16 +0530

    updated the gradient function: made theta(0)=0 for reg term

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,22 +19,25 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-% LINEAR REGRESSION: REGULARISED COST FUNCTION
+% --- LINEAR REGRESSION: REGULARISED COST FUNCTION ---
 
 % lamda : Regularisation parameter helps prevent overfitting by keeping all thetas small and smoothens the curve.
 %         Else, some thetas can assume large value and some small values, and may lead to overfitting.
 %         By using Regularisation, we are making sure all thetas are small and all features given equal importance,
 %         since its hard to say which feature is importatnt.
 
-cost_term = sum( ((X*theta) - y).^2 )
+sq_error_cost_term = sum( ((X*theta) - y).^2 )
 reg_term  = lambda * sum(theta(2:end).^2)
-J = (1/(2*m)) * (cost_term + reg_term)
+J = (1/(2*m)) * (sq_error_cost_term + reg_term)
 
 
-% LINEAR REGRESSION: REGULARISED GRADIENT FUNCTION
+% --- LINEAR REGRESSION: REGULARISED GRADIENT FUNCTION ---
+
 % The gradient is the partial derivative of J w.r.t theta(j)
+% theta(0) should not be considered for the Regularisation term. Regularisation term will be 0 at j=0.
 
-grad =  ( (1/m) * ( (transpose(X) * ((X*theta) - y ))  + (lambda * theta)) )
+theta_reg_term = [0; theta(2:end)]
+grad =  ( (1/m) .* ( (transpose(X) * ((X*theta) - y ))) )  + ((lambda / m) * theta_reg_term)
 
 
 % =========================================================================
