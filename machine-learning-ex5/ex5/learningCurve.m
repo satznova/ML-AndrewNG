c229d5fae4b9ef1f457d32a8504a9bb178799813
satznova <satznova@gmail.com>
2019-07-01 14:48:36 +0530

    implemented logic for learning curves

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -40,26 +40,20 @@ error_val   = zeros(m, 1);
 %       Do note that you will still need to use lambda when running
 %       the training to obtain the theta parameters.
 %
-% Hint: You can loop over the examples with the following:
-%
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
-%
 
-% ---------------------- Sample Solution ----------------------
+m_val = length(yval)
 
+for i = 1:m
+    % Compute train/cross validation errors using training examples
 
+    [theta] = trainLinearReg([ones(i, 1) X(1:i, :)], y(1:i), lambda);
 
+    J_train = linearRegCostFunction([ones(i, 1) X(1:i, :)], y(1:i), theta, 0);
+    J_val = linearRegCostFunction([ones(m_val, 1) Xval], yval, theta, 0);
 
-
-
-
-% -------------------------------------------------------------
+    error_train(i) =  J_train
+    error_val(i) = J_val
+endfor
 
 % =========================================================================
 
